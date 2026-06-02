bcea7c59dbf32565700ee24d2f4b610d27c1c253
Leandro Carvalho <151436121+LeandroMCarv@users.noreply.github.com>
2026-06-01 21:19:06 -0300

    feat: Entidade Categoria conforme modelado

---
+++ src/main/java/com/desafio/sgs/model/Categoria.java
@@ -0,0 +1,21 @@
+package com.desafio.sgs.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="categoria")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, length = 50)
+	private String nome;
+	
+	public Categoria() {}
+	
+	public Integer getId() { return id; }
+	public void setId(Integer id) { this.id = id; }
+	public String getNome() { return nome; }
+	public void setNome(String nome) { this.nome = nome; }
+}
