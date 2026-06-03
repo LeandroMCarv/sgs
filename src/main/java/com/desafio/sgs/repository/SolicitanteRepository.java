3f24d5f5382f85a86f93bff7da9ce1e268814965
Leandro Carvalho <151436121+LeandroMCarv@users.noreply.github.com>
2026-06-03 11:15:57 -0300

    feat: cria repositorio para a entidade Solicitante
    Interface que estende JpaRepository para fornecer operações de CRUD automáticas para a tabela de solicitantes.

---
+++ src/main/java/com/desafio/sgs/repository/SolicitanteRepository.java
@@ -0,0 +1,8 @@
+package com.desafio.sgs.repository;
+
+import com.desafio.sgs.model.Solicitante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SolicitanteRepository extends JpaRepository<Solicitante, Integer>{}
